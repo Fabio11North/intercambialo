d8c02e1168017e6cef3730014bd9b6d84f0c8f27
Yuu <84649842+Fabio11North@users.noreply.github.com>
2022-01-03 17:37:09 -0300

    Add files via upload

---
+++ pages/hola/index.js
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button';
+import { FaBars } from "react-icons/fa";
+
+export default function Hola() {
+  return (
+    <div>
+      <Button variant='info' className='text-white'>Hola</Button>
+    </div>
+  )
+}
